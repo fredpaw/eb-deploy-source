42b4cd9a9380fa51d882f57abb8b9b962c61d286
Fred Zhou <fredpaw@gmail.com>
2020-08-15 00:28:37 +1000

    test success

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 app = express();
 
 app.get("/", (req, res) => {
-  res.status(500).send("This is homepage.");
+  res.send("This is homepage.");
 });
 
 module.exports = app;
